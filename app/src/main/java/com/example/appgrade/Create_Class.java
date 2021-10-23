e76771d619ca967ebdef465f44b154f2d09dc25a
Paul Arnold <paularnoldsimbajon@gmail.com>
2021-10-24 05:22:29 +0800

    5th push

--- app/src/main/java/com/example/appgrade/Create_Class.java
+++ app/src/main/java/com/example/appgrade/Create_Class.java
@@ -3,12 +3,32 @@ package com.example.appgrade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Create_Class extends AppCompatActivity {
+public class Create_Class extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create__class);
+
+        Spinner spinner = findViewById(R.id.LevelingGrade);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Level, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String gradelvl = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
