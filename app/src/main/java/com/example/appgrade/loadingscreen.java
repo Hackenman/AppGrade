70e9716113de97f0b2b1e6f704ae72a0e20a97c9
ploys <paularnoldsimbajon@gmail.com>
2022-02-10 12:56:58 +0800

    17th commit

---
+++ app/src/main/java/com/example/appgrade/loadingscreen.java
@@ -0,0 +1,27 @@
+package com.example.appgrade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class loadingscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loadingscreen);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(loadingscreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+}
